d4d4446a8b23748d665af3412e38fd32a1b77cb1
doricigor <igor@forwardslashny.com>
2020-07-13 19:37:01 +0200

    Header, footer, banner done

--- index.php
+++ index.php
@@ -5,9 +5,15 @@
 ?>
     <div class="site-content">
         <main class="site-main">
-            <div class="container">
-                
-            </div>
+           <div class="banner">
+                <div class="container">
+                    <div class="banner__content">
+                        <img class="banner__logo" src="/img/logo.png" alt="Logo">
+                        <h1 class="banner__title page-title">A place to work <br> and network.</h1>
+                        <a class="banner__link" href="javascript:;">Schedule a tour</a>
+                    </div>
+                </div>
+           </div>
         </main>
     </div>
 <?php
