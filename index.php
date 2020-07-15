da00358bff097c3c41c11f61c69a01adbb173a8c
doricigor <igor@forwardslashny.com>
2020-07-15 19:18:14 +0200

    Basic-block, Story-block

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 ?>
     <div class="site-content">
         <main class="site-main">
+            <!-- Banner -->
            <div class="banner">
                 <div class="container">
                     <div class="banner__content">
@@ -13,6 +14,52 @@
                         <a class="banner__link" href="javascript:;">Schedule a tour</a>
                     </div>
                 </div>
+           </div>
+
+           <!-- Basic block -->
+           <div class="basic-block">
+                <div class="container">
+                    <h2 class="basic-block__title">Introducing <span class="basic-block__title-bold">E</span><span class="basic-block__title-tiny">S</span><sup>2</sup><span class="basic-block__title-small">tm</span></h2>
+                    <span class="basic-block__subtitle">Northwest Indiana's premier workspace.</span>
+                </div>
+           </div>
+
+           <!-- Story block -->
+           <div class="story-block">
+               <div class="container container--alt story-block__container">
+                   <div class="row">
+                        <div class="col-lg-6 story-block__img-wrapper">
+                            <img class="story-block__img" src="img/story1.png" alt="Story img">
+                        </div>
+                        <div class="col-lg-6 story-block__content-wrapper">
+                            <div class="story-block__content">
+                                <h2 class="story-block__content-title section-title section-title--line">Private</h2>
+                                <span class="story-block__content-subtitle">For peace and quiet</span>
+                                <p class="story-block__content-text">Choose from up to 59 private office spaces in sizes that fit your business—and make it easy to grow.</p>
+                                <a class="story-block__content-link" href="javascript:;">Learn more</a>
+                            </div>
+                        </div>
+                   </div>
+               </div>
+           </div>
+
+           <!-- Story block -->
+           <div class="story-block">
+               <div class="container container--alt story-block__container">
+                   <div class="row">
+                        <div class="col-lg-6 story-block__img-wrapper">
+                            <img class="story-block__img" src="img/story2.png" alt="Story img">
+                        </div>
+                        <div class="col-lg-6 story-block__content-wrapper">
+                            <div class="story-block__content">
+                                <h2 class="story-block__content-title section-title section-title--line">Shared</h2>
+                                <span class="story-block__content-subtitle">For peace and quiet</span>
+                                <p class="story-block__content-text">Choose from up to 59 private office spaces in sizes that fit your business—and make it easy to grow.</p>
+                                <a class="story-block__content-link" href="javascript:;">Learn more</a>
+                            </div>
+                        </div>
+                   </div>
+               </div>
            </div>
         </main>
     </div>
